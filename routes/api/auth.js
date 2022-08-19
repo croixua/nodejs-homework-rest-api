39043952400951fc4c1235d30b84abccb1462a64
Anton Kit <croixua@gmail.com>
2022-08-19 23:13:40 +0300

    added uploading avatars

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cntrl = require('../../controllers/auth/');
-const { auth } = require('../../middlewares');
+const { auth, upload } = require('../../middlewares');
 const { cntrlWrapper } = require('../../helpers');
 
 const router = express.Router();
@@ -9,5 +9,11 @@ router.post('/signup', cntrlWrapper(cntrl.signup));
 router.post('/login', cntrlWrapper(cntrl.login));
 router.get('/logout', auth, cntrlWrapper(cntrl.logout));
 router.get('/current', auth, cntrlWrapper(cntrl.getCurrent));
+router.patch(
+  '/avatars',
+  auth,
+  upload.single('avatar'),
+  cntrlWrapper(cntrl.setAvatar),
+);
 
 module.exports = router;
